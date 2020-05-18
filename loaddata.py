20c910b8711f2c43d4667483d65cafadace74f03
svejlgaard <fgj787@alumni.ku.dk>
2020-05-18 18:15:57 +0200

    Added feature to load all data in the data directory

--- loaddata.py
+++ loaddata.py
@@ -4,3 +4,14 @@ import os, contextlib, sys
 
 # Sets the directory to the current directory
 os.chdir(sys.path[0])
+
+def loaddata(directory):
+    data_dict = dict()
+    for filename in os.listdir(directory):
+        raw_data = np.genfromtxt(f'{directory}/{filename}', delimiter=',', names=True)
+        data_matrix = np.genfromtxt(f'{directory}/{filename}', delimiter=',')
+        date = filename.split(sep='_')[2]
+        data_dict.update({f'{date}_data': data_matrix})
+        data_dict.update({f'{date}_features': list(raw_data.dtype.names)})
+
+loaddata('data')
