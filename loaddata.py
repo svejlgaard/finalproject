f1cee3b8dab15eccf65c539a73ddfb4e3e3414a5
svejlgaard <fgj787@alumni.ku.dk>
2020-05-20 12:15:38 +0200

    Hardcoding :///

--- loaddata.py
+++ loaddata.py
@@ -7,14 +7,22 @@ os.chdir(sys.path[0])
 
 data_dict = dict()
 
+user_name = input('What is your name? [Simone/Jonathan/Marcus/Runi]')
+
+if user_name == 'Simone':
+    print('Welcome, master!')
+elif user_name == 'Jonathan':
+    directory = 'C:\Users\jonat\Documents\GitHub\finalproject'
+elif user_name == 'Runi':
+    directory = 'D:\Sapientia\Dropbox\Fysik på KU\Big Data Analysis\Final Project'
+elif user_name == 'Marcus':
+    user_name == 
+
 def loaddata(directory):
-    for filename in os.listdir(directory):
-        raw_data = np.genfromtxt(f'{directory}/{filename}', delimiter=',', names=True)
-        data_matrix = np.genfromtxt(f'{directory}/{filename}', delimiter=',')
-        date = filename.split(sep='_')[2]
-        data_dict.update({f'{date}_data': data_matrix})
-        data_dict.update({f'{date}_features': list(raw_data.dtype.names)})
-
+        for filename in os.listdir(directory):
+            raw_data = np.genfromtxt(f'{directory}/{filename}', delimiter=',', names=True)
+            data_matrix = np.genfromtxt(f'{directory}/{filename}', delimiter=',')
+            date = filename.split(sep='_')[2]
+            data_dict.update({f'{date}_data': data_matrix})
+            data_dict.update({f'{date}_features': list(raw_data.dtype.names)})
 loaddata('data')
-
-print(data_dict)
